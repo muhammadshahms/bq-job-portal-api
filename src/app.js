793c41291e0d0193f3ea66845b7a2cb80dfe80e5
Hamza Ansari <hmansari2020@gmail.com>
2024-09-30 21:03:37 +0500

    Your commit message

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import { errorMiddleware } from "./middlewares/error.middleware.js"
 const app = express();
 app.use(cors(
     {
-        origin: "https://bq-job-portal-api.vercel.app/",
+        origin: "*",
     }
 ));
 
