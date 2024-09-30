bc2ac594c0700df1f03c488f8074bdd78c74c91d
Hamza Ansari <hmansari2020@gmail.com>
2024-09-30 20:38:49 +0500

    Your commit message

--- src/app.js
+++ src/app.js
@@ -4,24 +4,19 @@ import cors from 'cors';
 import { errorMiddleware } from "./middlewares/error.middleware.js"
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: "*",
+    }
+));
 
-const corsOptions = {
-    origin: 'https://your-frontend-domain.com', // Replace with your frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    credentials: true, // Allow credentials (cookies, authorization headers)
-  };
-  
-
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
 
-import { userRoutes, companyRoutes, jobRoutes} from './routes/index.js'
+import { userRoutes, companyRoutes, jobRoutes } from './routes/index.js'
 
 app.use('/api/v1/user', userRoutes)
 app.use('/api/v1/company', companyRoutes)
